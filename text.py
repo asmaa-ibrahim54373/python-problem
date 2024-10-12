46a2addaee0df19295c5f2ba4b64b41d0e78d622
asmaa-ibrahim54373 <asmaayrgj9@gmail.com>
2024-10-12 15:09:30 +0300

    Add files via upload

---
+++ text.py
@@ -0,0 +1,10 @@
+#nested lope
+people=["asmaa","ahmed","samy","said"]
+skills=["html","css","js"]    
+
+for name in people :
+    
+    print(f"{name} skills is: ")
+    
+    for skill in skills :
+        print(skill)
