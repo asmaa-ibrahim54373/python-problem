86ac8837f803208a3e459254d61174386b75f98b
asmaa-ibrahim54373 <asmaayrgj9@gmail.com>
2024-10-12 16:06:59 +0300

    Update text.py

--- text.py
+++ text.py
@@ -1,10 +1,7 @@
-#nested lope
-people=["asmaa","ahmed","samy","said"]
-skills=["html","css","js"]    
+#question 1
 
-for name in people :
+#Write a program that prints the multiplication table for 6 as follows: 1 x 6 = 6 2 x 6 = 12 3 x 6 = 18 ……. 12 x 6 = 72 
+for i in range(1,13):
+   print(f"{i} x 6 = {i * 6}")
+
     
-    print(f"{name} skills is: ")
-    
-    for skill in skills :
-        print(skill)
